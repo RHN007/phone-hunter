2f2f6886565ef224de5ce945966b13ead2315ab8
Rakibul Hassan Nayon <rakibul.nayon@outlook.com>
2022-03-01 18:56:39 +0600

    adding info of phone

--- js/api.js
+++ js/api.js
@@ -4,5 +4,27 @@ const allPhones = () => {
     // console.log(url);
     fetch(url)
         .then((res) => res.json())
-        .then((data) => console.log(data));
+        .then((info) => showPhoneDtails(info.data));
+};
+
+const showPhoneDtails = (phones) => {
+    for (const phone of phones) {
+        const parent = document.getElementById("phone-container");
+        const div = document.createElement("div");
+        div.innerHTML = ` 
+        <div class="col">
+                <div class="card">
+                    <img src="${data.image}" class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title">${data.brand}</h5>
+                        <h6>Phone Name: ${data.phone_name} </h6>
+                        <p class="card-text">  </p>
+                        <div class="button">
+                              <button onclick="details('${data.slug}')" class="btn btn-success">Details</button>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+        parent.appendChild(div);
+    }
 };
